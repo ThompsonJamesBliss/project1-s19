db1240cc237f945be9001e34ee86f2f8025c467e
Thompson Bliss <42723197+ThompsonJamesBliss@users.noreply.github.com>
2019-04-08 16:05:07 -0400

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -51,7 +51,7 @@ engine = create_engine(DATABASEURI)
 
 
 # Here we create a test table and insert some values in it
-engine.execute("""SELECT * FROM Manager""")
+
 
 engine.execute("""DROP TABLE IF EXISTS test;""")
 engine.execute("""CREATE TABLE IF NOT EXISTS test (
@@ -122,10 +122,10 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM test")
+  cursor = g.conn.execute("SELECT name FROM BRANCH")
   names = []
   for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
+    names.append(result['address'])  # can also be accessed using result[0]
   cursor.close()
 
   #
