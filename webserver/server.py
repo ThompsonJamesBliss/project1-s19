8030776536a831ceaeeb12e7d99f2ac2c2a45670
Thompson Bliss <42723197+ThompsonJamesBliss@users.noreply.github.com>
2019-04-15 21:27:01 -0400

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -38,8 +38,8 @@ app = Flask(__name__, template_folder=tmpl_dir)
 # For your convenience, we already set it to the class database
 
 # Use the DB credentials you received by e-mail
-DB_USER = "pil2104"#os.environ['username']
-DB_PASSWORD = "2DJrhu9AoT" #os.environ['password']
+DB_USER = os.environ['username']
+DB_PASSWORD = os.environ['password']
 
 DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
 
